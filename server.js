f41e09f28b7d39fc08ee5e4460690b03bcbb0424
Diego Lehyt <diegolehy00@gmail.com>
2020-06-10 21:28:00 -0400

    Robo 3T connection added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // mongoose connection 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds119489.mlab.com:19489/heroku_cq6llj7m", { useNewUrlParser: true });
 
 // Import models (workout)
 const db = require("./models");
